3c5023e04879d618ec9b61d717e7430d4c328177
jlal1226 <jlal1226@naver.com>
2022-01-22 10:14:58 +0900

    baekjoon 2292

---
+++ src/BaekJoon/Honeycomb_2292.java
@@ -0,0 +1,13 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Honeycomb_2292 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+
+    }
+}
