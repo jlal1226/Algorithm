a83c48a3ec485dd668f0e81434e3b009177b3c68
jlal1226 <jlal1226@naver.com>
2022-02-26 18:34:04 +0900

    baekjoon 1018 수정

--- src/BaekJoon/ColoringChessBoard_1018.java
+++ src/BaekJoon/ColoringChessBoard_1018.java
@@ -3,69 +3,58 @@ package BaekJoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class ColoringChessBoard_1018 {
     static int min = 64;
-    public static void minNum (int cnt) {
-        if (cnt < min)
-            min = cnt;
-    }
+    public static boolean[][] arr;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] str;
         str = br.readLine().split(" ");
         int N = Integer.parseInt(str[0]);
         int M = Integer.parseInt(str[1]);
-
-        // 2차원 배열 생성
-        String[][] chess = new String[N][M];
+        arr = new boolean[N][M];
 
         for (int i = 0; i < N; i++) {
             String input = br.readLine();
             for (int j = 0; j < M; j++) {
-                chess[i] = input.split("");
-            }
-        }
-        // 시작이 B냐 W냐에 따라 홀수 짝수 다르게 판단하면 될듯?
-        for (int i = 0; i <= N - 8; i++) {
-            for (int j = 0; j <= M - 8; j++) {
-                int cnt = 0;
-                if ( chess[i][j].equals("W")) {
-                    // 8 * 8 검사
-                    for (int k = i; k < 8 + i; k ++) {
-                        for (int l = j; l < 8 + j; l++) {
-                            if ( (k + l) % 2 == 0) {
-                                if (chess[k][l].equals("B"))
-                                    cnt++;
-                            }else {
-                                if (chess[k][l].equals("W"))
-                                    cnt++;
-                            }
-                        }
-                    }
-
+                if (input.charAt(j) == 'W') {
+                    arr[i][j] = true; // W일때 true
                 }else {
-                    // 8 * 8 검사
-                    for (int k = i; k < 8 + i; k ++) {
-                        for (int l = j; l < 8 + j; l++) {
-                            if ( (k + l) % 2 == 0) {
-                                if (chess[k][l].equals("W"))
-                                    cnt++;
-                            }else {
-                                if (chess[k][l].equals("B"))
-                                    cnt++;
-                            }
-                        }
-                    }
-                    minNum(cnt);
+                    arr[i][j] = false; // B일때 false
                 }
             }
         }
-        if (min == 64) {
-            System.out.println(0);
-        }else {
-            System.out.println(min);
+        int N_row = N - 7;
+        int M_col = M - 7;
+
+        for (int i = 0; i < N_row; i++) {
+            for (int j = 0; j < M_col; j++) {
+                find(i,j);
+            }
         }
+        System.out.println(min);
+    }
+
+    static void find(int x, int y) {
+        int end_x = x + 8;
+        int end_y = y + 8;
+        int cnt = 0;
+
+        boolean TF = arr[x][y]; // 첫 번째 칸의 색
+
+        for (int i = x; i < end_x; i++) {
+            for (int j = y; j < end_y; j++) {
+                if (arr[i][j] != TF) {
+                    cnt++;
+                }
+                TF = (!TF); // 다음 칸은 다른색이여야함
+            }
+            TF = (!TF);
+        }
+        cnt = Math.min(cnt, 64 - cnt);
+
+        min = Math.min(min, cnt);
     }
 }
