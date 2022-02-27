5ccc48271fbfbee70cf98bbafa8d388e67900dfc
jlal1226 <jlal1226@naver.com>
2022-02-27 23:56:22 +0900

    baekjoon 15649 BackTracking

---
+++ src/BaekJoon/NandM1_15649.java
@@ -0,0 +1,41 @@
+package BaekJoon;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class NandM1_15649 {
+    static boolean[] check;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        check = new boolean[N];
+        arr = new int[M];
+        dfs(N, M, 0);
+        System.out.println(sb);
+    }
+
+    static void dfs(int N, int M, int depth) {
+        if (depth == M) {
+            for (int val : arr)
+                sb.append(val).append(" ");
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < N; i++)  {
+            if(!check[i]) {
+                check[i] = true;
+                arr[depth] = i + 1;
+                dfs(N, M, depth + 1);
+                check[i] = false;
+            }
+        }
+    }
+}
