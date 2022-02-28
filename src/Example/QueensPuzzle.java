9cd68a625505eac6c71610ef2d123981c2539a89
jlal1226 <jlal1226@naver.com>
2022-02-28 23:06:16 +0900

    8 Queens Puzzle 설명 추가

--- src/Example/QueensPuzzle.java
+++ src/Example/QueensPuzzle.java
@@ -1,21 +1,22 @@
 package Example;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
 public class QueensPuzzle {
     static boolean[] flag_a = new boolean[8];
     static boolean[] flag_b = new boolean[15];
     static boolean[] flag_c = new boolean[15];
     static int cnt = 0;
     public static void main(String[] args) {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = 8;
         queen(0);
         System.out.println(cnt);
     }
 
     static void queen(int i) {
+        /*
+        [ 백트랙킹 알고리즘 ] -> 특정 조건을 만족하는 모든 경우의 수를 검색하는 알고리즘 (가지치기)
+        1. 각 열에 퀸을 1개만 배치한다.
+        2. 각 행에 퀸을 1개만 배치한다.
+        3. 왼쪽 대각선 방향, 오른쪽 대각선 방향으로 겹치지 않는다.
+         */
         // i : 열, j : 행
         for(int j = 0; j < 8; j++){
             // 행, 대각선 2개에 겹치지 않도록
