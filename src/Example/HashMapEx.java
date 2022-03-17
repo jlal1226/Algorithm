a877141f77c22fa12761b511301b45eb5a235ec9
jlal1226 <jlal1226@naver.com>
2022-03-17 23:32:59 +0900

    HashMap 메소드 정리

--- src/Example/HashMapEx.java
+++ src/Example/HashMapEx.java
@@ -8,29 +8,37 @@ public class HashMapEx {
         Map<String, Integer> map = new HashMap();
 
         // put(), get()
-        map.put("A", 100);
-        map.put("B", 200);
-        map.put("C", 300);
-        map.put("D", 400);
+        map.put("A", 1);
+        map.put("B", 1);
+        map.put("C", 1);
+        map.put("D", 1);
 
-        System.out.println(map);
         System.out.println(map.get("A"));
         System.out.println(map.get("B"));
         System.out.println(map.get("C"));
 
+        System.out.println(map);
+
         // containsKey()
         if (!map.containsKey("Z")) {
-            map.put("Z", 100);
+            map.put("Z", 1);
+        }
+        //System.out.println(map);
+
+        // putIfAbsent()
+        map.putIfAbsent("Z",3);
+
+        // getOrDefault() -> 동일한 키 찾아서 값을 덮어씀
+        String[] arr = {"A", "B", "C"};
+        for (String key : arr) {
+            map.put(key, map.getOrDefault(key, 0) + 1);
         }
         System.out.println(map);
 
-        // putIfAbsent()
-        map.putIfAbsent("Z",300);
-        System.out.println(map);
 
-        Map<String, Integer> map2 = new HashMap();
-        map2.put("test1", 123);
-        map2.put("test2", 456);
+        Map<String, Integer> map2 = new HashMap<>();
+        map2.put("test1", 9);
+        map2.put("test2", 9);
 
         map.putAll(map2);
         System.out.println(map);
@@ -39,5 +47,7 @@ public class HashMapEx {
         for (String str : map.keySet()) {
             System.out.print(str + " ");
         }
+
+
     }
 }
