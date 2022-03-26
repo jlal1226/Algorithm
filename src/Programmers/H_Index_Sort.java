261674c538134b8284471c281131822c18df72a1
jlal1226 <jlal1226@naver.com>
2022-03-26 15:48:21 +0900

    Programmers H-Index

---
+++ src/Programmers/H_Index_Sort.java
@@ -0,0 +1,33 @@
+package Programmers;
+
+import java.util.Arrays;
+
+public class H_Index_Sort {
+    public static void main(String[] args) {
+        int[] arr = {1000,1000,1000,1000,1000};
+        System.out.print(solution(arr));
+    }
+    public static int solution(int[] citations) {
+        int answer = 0;
+        int n = citations.length;
+        Arrays.sort(citations);
+        for (int i = n; i >= 0; i--) {
+            int count = 0;
+            for (int j = n - 1; j >= 0; j--) { // 큰 수 부터 탐색
+                if (citations[j] < i) {
+                    break;
+                }
+                if (count > i) {
+                    break;
+                }
+                count++;
+            }
+            if (i <= count) {
+                answer = i;
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
