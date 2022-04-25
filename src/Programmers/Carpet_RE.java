158509e676be222c421c6132d02264f53900f737
jlal1226 <jlal1226@naver.com>
2022-04-25 18:41:52 +0900

    [Programmers] 완전탐색-카펫 Carpet_RE

---
+++ src/Programmers/Carpet_RE.java
@@ -0,0 +1,21 @@
+package Programmers;
+
+public class Carpet_RE {
+    public static void main(String[] args) {
+        solution(24, 24);
+    }
+
+    public static int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+        // 노란색의 약수
+        for (int row = 1; row*row <= yellow; row++) {
+            int col = yellow / row;
+            if ((row + 2) * (col + 2) == brown + yellow) {
+                answer[0] = col + 2;
+                answer[1] = row + 2;
+            }
+        }
+
+        return answer;
+    }
+}
