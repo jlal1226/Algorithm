ade76dda1f6309928577c56817ccb4be82dfcd1a
jlal1226 <jlal1226@naver.com>
2022-05-06 23:40:06 +0900

    [Programmers] DFS/BFS Network

---
+++ src/Programmers/DFS_Network.java
@@ -0,0 +1,59 @@
+package Programmers;
+
+import java.util.LinkedList;
+import java.util.Stack;
+
+public class DFS_Network {
+    public static void main(String[] args) {
+        // {{1,1,0}, {1,1,1}, {0,1,1}}
+        // {{1,1,0}, {1,1,0}, {0,0,1}}
+        // {{1,1,1,0}, {1,1,1,0}, {1,1,1,0}, {0,0,0,1}} -> 2
+        int[][] computers = {{1,1,1,0}, {1,1,1,0}, {1,1,1,0}, {0,0,0,1}};
+        int n = 4;
+        int result = solution(n, computers);
+        System.out.println(result);
+    }
+
+    public static int solution(int n, int[][] computers) {
+        int answer = 0;
+
+        LinkedList<Integer>[] graph = new LinkedList[n];
+        boolean[] visited = new boolean[n];
+        // 그래프 생성
+        for (int i = 0; i < n; i++) {
+            graph[i] = new LinkedList<>();
+        }
+
+        // 노드 연결
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i != j && computers[i][j] == 1) {
+                    graph[i].addFirst(j);
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                Stack<Integer> stack = new Stack<>();
+                stack.push(i); // 0부터 시작
+
+                // DFS 탐색
+                while (!stack.isEmpty()) {
+                    int v = stack.pop();
+                    if (!visited[v]) {
+                        visited[v] = true; // 방문 처리
+                        for (int j = 0; j < graph[v].size(); j++) {
+                            int destination = graph[v].get(j);
+                            if (!visited[destination])
+                                stack.push(destination);
+                        }
+                    }
+                }
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+}
